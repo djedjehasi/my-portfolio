4dd28dd44bc05cb4bc05550fd4cd351dbbe5de70
Yosef Gehasy <djedjehasi@gmail.com>
2023-02-01 16:11:35 +0200

    adding header and about comppnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './compnents/header/Header'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
 
